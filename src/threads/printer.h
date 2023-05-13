5e9e4de4b3e8e2ffb1bca4ba1a1e013971066a37
Kacper Stojecki <mozgwfiolce@gmail.com>
2023-05-13 19:56:46 +0200

    Defined parameter structure for printer thread.

--- src/threads/printer.h
+++ src/threads/printer.h
@@ -1,8 +1,18 @@
 #ifndef PRINTER_H_INCLUDED
 #define PRINTER_H_INCLUDED
+#include <threads.h>
+#include "circbuf.h"
 
 
-// FIXME: Not yet implemented
+typedef struct PrinterThreadParams
+{
+	mtx_t* mutex;
+	CircularBuffer_t* buffer;
+}
+PrinterThreadParams_t;
+
+
+int PrinterThread(void* params);
 
 
 #endif // !PRINTER_H_INCLUDED
