61a83594b6ca1cb855265fd5c78bec276e11b965
Kacper Stojecki <mozgwfiolce@gmail.com>
2023-05-13 18:27:19 +0200

    Implemented reader thread.

--- src/threads/reader.c
+++ src/threads/reader.c
@@ -1,1 +1,71 @@
 #include "reader.h"
+#include <threads.h>
+#include <stdatomic.h>
+#include "procstat.h"
+#include "thread_utils.h"
+#include "log.h"
+
+
+#define SLEEP_TIME_SECONDS 1
+#define MUTEX_WAIT_TIME_MS 50
+
+
+int ReaderThread(void* rawParams)
+{
+	if (NULL == rawParams)
+	{
+		thrd_exit(-1);
+	}
+
+	ReaderThreadParams_t* params = (ReaderThreadParams_t*) rawParams;
+
+	// Only continue execution if kill switch hasn't been activated
+	while (false == Thread_getKillSwitchStatus())
+	{
+		// Load procstat from file
+		ProcStat_t* procStat = ProcStat_loadFromFile();
+
+		if (NULL == procStat)
+		{
+			// Procstat is unavailable, log error and try again in next iteration
+			Log(LLEVEL_ERROR, "reader: cannot load data from /proc/stat file");
+		}
+		else
+		{
+			// Procstat has been acquired, lock mutex on circular buffer and write
+			if (thrd_success == Mutex_tryLockMs(params->mutex, MUTEX_WAIT_TIME_MS))
+			{
+				// Mutex acquired, write procstat
+				CircularBuffer_write(params->dataBuffer, procStat);
+				// Unlock mutex so analyzer thread can access it
+				Mutex_unlock(params->mutex);
+				
+				Log(LLEVEL_DEBUG, "reader: procstat data sent");
+				Log(LLEVEL_TRACE, "reader: cpu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", 
+					procStat->cpuStats[0].values[0],
+					procStat->cpuStats[0].values[1],
+					procStat->cpuStats[0].values[2],
+					procStat->cpuStats[0].values[3],
+					procStat->cpuStats[0].values[4],
+					procStat->cpuStats[0].values[5],
+					procStat->cpuStats[0].values[6],
+					procStat->cpuStats[0].values[7],
+					procStat->cpuStats[0].values[8],
+					procStat->cpuStats[0].values[9]);
+			}
+			else
+			{
+				Log(LLEVEL_WARNING, "reader: couldn't acquire buffer mutex");
+			}
+
+			// Remove obsolete procstat object and move on
+			ProcStat_destroy(procStat);	
+		}
+
+		// Wait for next iteration
+		Thread_sleep(SLEEP_TIME_SECONDS);
+	}
+
+	// Exit as usual
+	thrd_exit(0);
+}
--- src/threads/reader.h
+++ src/threads/reader.h
@@ -1,8 +1,30 @@
 #ifndef READER_H_INCLUDED
 #define READER_H_INCLUDED
+#include <threads.h>
+#include "circbuf.h"
 
 
-// FIXME: Not yet implemented
+typedef struct ReaderThreadParams
+{
+	mtx_t* mutex;
+	CircularBuffer_t* dataBuffer;
+}
+ReaderThreadParams_t;
+
+
+/**
+ * \brief Thread function for reading /proc/stat file and parsing it's content
+ * into ProcStat_t structure.
+ * \details Thread will periodically read and parse /proc/stat file and write the result
+ * into circular buffer provided through params. Those writes will be only performed if provided
+ * mutex is available, as it will be locked for their entire duration.
+ * Field mutex of params should point to valid mutex which will be used for synchronizing
+ * operations on dataBuffer.
+ * Field dataBuffer of params should point to valid CircularBuffer_t structure capable of holding
+ * at least one ProcStat_t structure.
+ * \param params Pointer to valid ReaderThreadParams_t structure.
+*/
+int ReaderThread(void* params);
 
 
 #endif // !READER_H_INCLUDED
